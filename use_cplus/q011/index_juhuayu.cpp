a970ed9529840fa06f678a6fd0bfaa7e5f9bfd7f
juhuayu <15604481498@163.com>
2022-11-21 22:03:38 +0800

    q011-改进版

--- use_cplus/q011/index_juhuayu.cpp
+++ use_cplus/q011/index_juhuayu.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
  
-// C
+// C 20221118
 // 输入：[1,8,6,2,5,4,8,3,7]
 // 预计输出： 49
 // 超出时间限制
@@ -22,10 +22,32 @@ int maxArea(int* height, int heightSize) {
   return res_max;
 }
 
+// C 20221121
+// 执行用时：76 ms, 在所有 C 提交中击败了 80.40% 的用户
+// 内存消耗：11.4 MB, 在所有 C 提交中击败了 76.79% 的用户
+int maxArea_ameliorate(int* height, int heightSize) {
+  int i=0, j=heightSize-1, temp = 0, res_max = 0;
+  while (i<j) {
+    temp = (j - i) * (height[i]<height[j]?height[i]:height[j]);
+    if (temp > res_max)
+    {
+      res_max = temp;
+    }
+    if (height[i] > height[j])
+    {
+      j--;
+    } else {
+      i++;
+    }
+  }
+  return res_max;
+}
+
 int main()
 {
     int arr[] = {1,8,6,2,5,4,8,3,7};
-    cout << "result:" << maxArea(arr, 9) << endl;
+    // cout << "result:" << maxArea(arr, 9) << endl;
+    cout << "result:" << maxArea_ameliorate(arr, 9) << endl;
 
     getchar();
     return 0;
